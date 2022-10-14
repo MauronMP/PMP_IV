9c7ea78b5e62a1c5bfdc0db5279d3eb010149fc4
Antonio Ramón Sánchez Morales <antonioramonsm@gmail.com>
2022-10-14 09:28:58 +0200

    chore(models.fecha): added docstrings
    closes #14

--- pmp_iv/models/fecha.py
+++ pmp_iv/models/fecha.py
@@ -5,10 +5,12 @@ from pmp_iv.enums.day import Day
 from pmp_iv.enums.month import Month
 from pmp_iv.utils.validation import Validation
 
-
+'''Utilizado para segmentar la información'''
 @dataclass
 class Fecha:
+    '''Opciones posibles para segmentar información mediante meses'''
     mes: Month
+    '''Opciones posibles para segmentar información mediante día de la semana'''
     dia: Day
 
     def __post_init__(self):
