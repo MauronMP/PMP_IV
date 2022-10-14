6193fa8b847aaac5a3f5753a8840a3a29816024f
Antonio Ramón Sánchez Morales <antonioramonsm@gmail.com>
2022-10-14 09:36:52 +0200

    chore(models.estado): added docstrings
    closes #13

--- pmp_iv/models/estado.py
+++ pmp_iv/models/estado.py
@@ -3,12 +3,18 @@ from pmp_iv.config.model_validation import ModelValidation
 
 from pmp_iv.utils.validation import Validation
 
+'''Propiedades últiles para poder realizar el cálculo de los elementos de la clase fwi'''
 @dataclass
 class Estado:
+    '''Temperatura en la coordenada'''
     temperature: int
+    '''Humedad en la coordenada'''
     humidity: int
+    '''Densidad del viento en la coordenada'''
     wind: float
+    '''Cantidad de lluvia en la coordenada'''
     rain: float
+    '''Superficie de la coordenada'''
     surface: int
 
     def __post_init__(self):
