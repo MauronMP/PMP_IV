4f5a0c9a13eacbb2f57b1151d063867c9bd18b7b
MauronMP <pablo.mopi@gmail.com>
2022-11-03 15:52:47 +0100

    ✅ test(test_pmp_iv.py): Ahora analiza la sintaxis del directorio pmp_iv con exito.

--- tests/test_pmp_iv.py
+++ tests/test_pmp_iv.py
@@ -1,13 +1,4 @@
-import pmp_iv.utils.validation 
-import pmp_iv.models.fecha 
-import pmp_iv.models.estado 
-import pmp_iv.models.fwi 
-import pmp_iv.enums.day 
-import pmp_iv.enums.month
-import pmp_iv.models.coordenada
+import os
 
 def test():
-    assert pmp_iv.models.coordenada.Coordenada(5,5)
-    assert pmp_iv.models.fecha.Fecha(pmp_iv.enums.month.Month.julio,pmp_iv.enums.day.Day.L)
-    assert pmp_iv.models.estado.Estado(10,10,9,3,200)
-    assert pmp_iv.models.fwi.FWI(30.4,124.5,664.9,32.6)
+    os.system("python3 -m compileall pmp_iv")
