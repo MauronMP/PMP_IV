0efb5eb6a505847ac9d43eee8af7c0da69cfd069
MauronMP <pablo.mopi@gmail.com>
2022-11-16 14:30:46 +0100

    👷 build(regression && model.py): Delete, no son necesarios para #26

--- pmp_iv/enums/regression_algorithm.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
- 
-class Regression_algorithm(Enum):
-    Lasso = 'Lasso'
-    Ridge = 'Ridge'
-    RandomForestRegressor = 'RandomForestRegressor'
-    KNeighborsRegressor = 'KNeighborsRegressor'
